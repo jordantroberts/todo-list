d5fa32df5e49f9a7a31a3c55760a6b578b681396
Jordan Roberts <jordanroberts@talk21.com>
2019-06-02 11:28:08 +0100

    Added  in tests to stub user input

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -3,6 +3,11 @@ require 'todo'
 describe Todo do
   subject(:todo) { described_class.new }
 
+  before do
+    allow($stdout).to receive(:write)
+    allow(todo).to receive(:gets).and_return("Clean car\n")
+  end
+
   describe '#initialize' do
     it 'has an empty list to begin with' do
       expect(todo.list).to eq []
@@ -11,7 +16,7 @@ describe Todo do
 
   describe '#create' do
     it 'creates a new todo list item' do
-      expect(todo.create).to eq "Clean car" 
+      expect(todo.create).to eq "Clean car"
     end
   end
 
